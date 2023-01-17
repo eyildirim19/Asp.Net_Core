62a6d3f5a7ebf7ad007f830401e7cb076df43d9d
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-01-17 21:34:47 +0300

    Add project files.

---
+++ Giris/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Giris.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Default Action Indextir.
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        // bu metota httprequesti yapılabilir...
+        public string Index()
+        {
+            return "MERHABA ARKADAŞLAR. MVC'YE GİRİŞ YAPTIK...";
+        }
+
+        public IActionResult Sayfa()
+        {
+            return View();
+            // View(); ViewResult dönüştiplidir...Viewlar asp.net core'da html sayfalarını ifade eder. ancak bu html sayfaları .cshtml (csharphtml) uzantılıdır. 
+        }
+
+        // IActionResult => httpresponse type'dır...
+        public IActionResult Veri()
+        {
+            List<string> kisiler = new List<string>();
+            kisiler.Add("İsmail");
+            kisiler.Add("Berat");
+            kisiler.Add("Ekrem");
+            kisiler.Add("Merve");
+            //return kisiler;
+            return Json(kisiler);
+        }
+    }
+}
---
+++ Giris/Controllers/MesajController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Giris.Controllers
+{
+    public class MesajController : Controller
+    {
+        public int Islem()
+        {
+            return 2 + 2;
+        }
+
+        public string Test()
+        {
+            return "Merhaba";
+        }
+
+        public DateTime Tarih()
+        {
+            return DateTime.Now;
+        }
+    }
+}
