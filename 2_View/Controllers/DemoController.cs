0a197fda175c14c3807c45d3501e6ab72b078474
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-01-19 21:32:56 +0300

    Vİew yapısı ve razor syntax

---
+++ 2_View/Controllers/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2_View.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+// localhost:portno/Home
+// localhost:portno/Demo
---
+++ 2_View/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2_View.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Sayfa()
+        {
+            // Not=> View() metodu default ActionName.cshtml dosyasını döner
+            //return View(); //Sayfa.cshtml döner...
+            return View("biz"); // biz.cshtml döner...
+        }
+
+        // parametre alan actionmetot
+        // Url => Home/Eksin?tip=admin|user
+        public IActionResult Eksin(string tip)
+        {
+            if (tip == "admin")
+                return View(); // View dön
+            else if (tip == "user")
+                return Json("Hoş Geldin User"); // json dön
+            else
+                return RedirectToAction("Index"); // Action dön
+        }
+
+
+
+    }
+}
