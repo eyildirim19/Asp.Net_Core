6af9cbaae78a2f5f801f0ff75a1d50319f3af154
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-02-27 21:54:24 +0300

    hazır template giydirme

--- 13_AspNetCoreApp/Program.cs
+++ 13_AspNetCoreApp/Program.cs
@@ -12,10 +12,25 @@ builder.Services.AddDbContext<NorthwindContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("myCon"));
 });
 
+//builder.Services.AddIdentity<>
+builder.Services.ConfigureApplicationCookie(conf =>
+{
+    conf.LoginPath = "~/Account/LogIn";
+});
+
 var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+
+
+app.UseAuthentication(); // oturum middleware
+app.UseAuthorization(); // yetki middleware
+
+app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
 
 app.MapControllerRoute("default",
     "{controller=home}/{action=index}");
